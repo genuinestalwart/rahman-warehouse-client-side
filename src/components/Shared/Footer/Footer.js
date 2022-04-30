81f668d4ea9ed185ce3b2d82997558fee286f5ef
genuinestalwart <genuinestalwart@gmail.com>
2022-04-30 22:03:54 +0600

    footer added

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return (
-        <div>
-            This is footer
-        </div>
+        <footer className='footer text-center'>
+            <div>
+                <Link className='link p-2' to='/inventory'>Inventory</Link>
+                <Link className='link p-2' to='/blogs'>Blogs</Link>
+                <Link className='link p-2' to='/login'>Log in</Link>
+            </div>
+            <div className='my-3'>
+                <FontAwesomeIcon className='d-inline-block fa-icon m-2 p-2 rounded-circle' icon={faFacebook}></FontAwesomeIcon>
+                <FontAwesomeIcon className='d-inline-block fa-icon m-2 p-2 rounded-circle' icon={faInstagram}></FontAwesomeIcon>
+                <FontAwesomeIcon className='d-inline-block fa-icon m-2 p-2 rounded-circle' icon={faTwitter}></FontAwesomeIcon>
+                <FontAwesomeIcon className='d-inline-block fa-icon m-2 p-2 rounded-circle' icon={faLinkedin}></FontAwesomeIcon>
+            </div>
+            <p className='fw-bold mb-0'>&copy;2022 <span>Rahman Warehouse</span>, all rights reserved.</p>
+        </footer>
     );
 };
 
